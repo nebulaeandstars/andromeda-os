23d81212398a61fd8001a8d24dcbddf04c64bd28
Laura Powles <laura@laurapowles.com>
2021-10-28 08:11:20 +1100

    refactored code into a library

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
-mod serial;
-mod vga;
 
 use core::panic::PanicInfo;
 
-use vga::VGA_WRITER;
+use andromeda_os::println;
+use andromeda_os::vga::VGA_WRITER;
 
 #[no_mangle]
 pub extern "C" fn _start() -> !
@@ -44,23 +42,6 @@ fn panic(info: &PanicInfo) -> !
     loop {}
 }
 
-pub trait Test
-{
-    fn run(&self) -> ();
-}
-
-impl<T> Test for T
-where
-    T: Fn(),
-{
-    fn run(&self)
-    {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Test])
 {
@@ -79,24 +60,4 @@ fn panic(info: &PanicInfo) -> !
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode
-{
-    Success = 0x10,
-    Failed  = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) -> !
-{
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-
-    loop {}
 }
